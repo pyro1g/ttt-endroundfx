929c85fe49bfc1c87d31235c9229cc9db6dd2d3e
pyro <kylepyro1990@hotmail.com>
2017-12-22 00:26:38 -0500

    Update sv_endroundfx.lua

--- lua/autorun/endroundfx/sv_endroundfx.lua
+++ lua/autorun/endroundfx/sv_endroundfx.lua
@@ -2,11 +2,6 @@
 	Script: EndRoundFX, Serverside
 
 	DarkPyro Gaming Servers, https://dpg.tf/
-	Copyright © 2018, all rights reserved.
-
-	Any unauthorized distribution or modification
-	of the following code will result in persecution
-	of said offender to the fullest extent of the law.
 ]]--
 
 util.AddNetworkString "EndRoundFX"
