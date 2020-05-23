30d4835151263c21e3ac79398d6fe7781ae7ca73
pyro <kylepyro1990@hotmail.com>
2020-05-23 06:16:52 -0400

    Update sh_endroundfx.lua

--- lua/autorun/sh_endroundfx.lua
+++ lua/autorun/sh_endroundfx.lua
@@ -7,19 +7,28 @@
 AddCSLuaFile()
 
 resource.AddFile "resource/fonts/bebasneue.ttf"
+
 RunConsoleCommand( "ttt_postround_dm", "1" )
 
 ENDROUNDFX = ENDROUNDFX or {}
 
+function ENDROUNDFX:Print( text )
+	MsgC( Color( 255, 255, 0 ), "[EndRoundFX] ", Color( 255, 255, 255 ), text .. "\n" )
+end
+
 function ENDROUNDFX:Load()
+	self:Print( "Initializing..." )
+
 	if SERVER then
 		include "endroundfx/sv_endroundfx.lua"
 		AddCSLuaFile "endroundfx/cl_endroundfx.lua"
+
+		self:Print( "Loaded serverside files..." )
 	else
 		include "endroundfx/cl_endroundfx.lua"
 	end
 
-	MsgC( Color( 0, 148, 255 ), "[EndRoundFX] ", Color( 255, 255, 255 ), "Initialized!" .. "\n" )
+	self:Print( "Successfully initialized!" )
 end
 
 ENDROUNDFX:Load()
